5f12dd186b2894605c17c89c58114fbfd1680c67
imzero238 <nayoung8142@gmail.com>
2025-01-16 11:04:30 +0900

    Initial commit

---
+++ src/main/java/com/nayoung/dataencryptiontest/DataEncryptionTestApplication.java
@@ -0,0 +1,13 @@
+package com.nayoung.dataencryptiontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataEncryptionTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataEncryptionTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nayoung/dataencryptiontest/DataEncryptionTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nayoung.dataencryptiontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataEncryptionTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
